94f538bdf41fe2a56784c72f68393cc063ac0acc
TheRedCreator <schoolkha@hotmail.com>
2023-06-10 12:12:36 +0100

    Projectile movement with bugs

--- Assets/Others/Scripts/ProjectileManager.cs
+++ Assets/Others/Scripts/ProjectileManager.cs
@@ -8,65 +8,93 @@ public class ProjectileManager : MonoBehaviour
     public float selfRotate;
     public Transform target; // The target object to follow 
     public ThirdPersonCharacterController character;
-    public float normalFollowSpeed = 5f; // The speed at which the object follows the target
-    public float recoveryFollowSpeed = 5f; // The speed at which the object gets back to the target after the throw
-    public float attackFollowSpeed = 10f;
     public float aimingOffset = 2f;
 
-    public bool isCharging = false; // if the ball is ready to be thrown
-    public bool isReady = true;
-    public bool isShooting = false;
 
-    public float smoothSpeed = 5f; // The speed at which the object moves towards the target
-    public float range = 5f; // The distance from the object to the target position
+    public bool willAttack = false; // if the ball is ready to be thrown
+    public bool willRecover = false; // if the ball is ready to recover
+    public bool isReady = true; // if the ball is ready to be charged
+    public bool isAiming;
+    public bool canTravel = true; // if the ball can still move towards where it should go depending on the state
 
+    public float recoverySpeed = 4f;
+    public float followSpeed = 2f;
+    public float attackSpeed = 10f;
+    public float defaultSpeed = 5f; // The speed at which the object moves towards a destination
+    public float range = 10f; // The distance from the object to the target position
+    public float power = 0; // multiplier that goes up when charging
+
+    private Vector3 startingPosition;
     private Vector3 chargingPosition;
     private Vector3 recoveryPosition;
-    private Vector3 attackPosition;
-
-    private Vector3 targetPosition;
+    private Vector3 attackVector;
 
     private void Start()
     {
         chargingPosition = target.position + Vector3.right * aimingOffset;
-        attackPosition = transform.position + character.transform.forward * range;
+        attackVector = character.transform.forward * range;
         recoveryPosition = target.position;
     }
     private void Update()
     {
-        
 
+         isAiming = character.isAiming;
 
-        /*
-        if (isReady)
+        if (isAiming && isReady)
         {
-            if (character.isAiming)
+        // Charging State: while right click is hold & the ball is ready
+            if (canTravel)
             {
-                // the ball travels to the side of the player by an X offset
-                CanTravelTowards(chargingPosition, normalFollowSpeed);
-                isCharging = true;
-            }
-            else if (isShooting)
-            {
-                // the ball travels to destination to attack
-                isShooting = CanTravelTowards(attackPosition, attackFollowSpeed);
+                canTravel = CanTravelTowards(chargingPosition, defaultSpeed);
+                power += 0.1f;
+                startingPosition = transform.position;
+                willAttack = true;
             }
             else
             {
-                // the ball travels back towards target
-                CanTravelTowards(recoveryPosition, recoveryFollowSpeed);
+                isReady = false;
+                canTravel = true;
             }
         }
         else
         {
-            // the ball travels back towards target
-            isReady = !CanTravelTowards(recoveryPosition, recoveryFollowSpeed);
-            if (isReady)
+            if (willAttack)
+            // Attacking State: after Charging State && ( right click was released || or || ball arrived to chargingPosition )
             {
-                isCharging = false;
+                if (canTravel)
+                {
+                    canTravel = CanTravelTowards(startingPosition + attackVector * power, attackSpeed);
+                    willRecover = true;
+                }
+                else if (isAiming) // To start recovery press right click
+                {
+                    willAttack = false;
+                    canTravel = true;
+                }
+            }
+            else if (willRecover)
+            // Revcovery State: after Attacking State & right click pressed
+            {
+                if (canTravel)
+                {
+                    canTravel = CanTravelTowards(recoveryPosition, recoverySpeed);
+                }
+                else
+                {
+                    willRecover = false;
+                    isReady = true;
+                    canTravel = true;
+                }
+            }
+            else if (isReady)
+            // Neutral State: if not in any other state - ball follows target
+            {
+                willRecover = false;
+                willAttack = false;
+                CanTravelTowards(target.position, followSpeed);
+                canTravel = true;
             }
         }
-        */
     }
 
     private bool CanTravelTowards(Vector3 destination, float speed)
