5058a45b7c686f6cc2a13be16ac334e2d876f00b
TheRedCreator <schoolkha@hotmail.com>
2023-07-02 15:13:23 +0100

    Build Settings

---
+++ Assets/Others/Scripts/Exit_script.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Exit_script : MonoBehaviour
+{
+    public void button_exit()
+    {
+
+        Application.Quit();
+
+    }
+}
---
+++ Assets/Others/Scripts/LoadScene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadScene : MonoBehaviour
+{
+    public float CinematicTime;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Cinematic1");
+        {
+            StartCoroutine(LoadNextSceneAfter());
+        }
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    IEnumerator LoadNextSceneAfter()
+    {
+        yield return new WaitForSeconds(14);
+        LoadNextScene();
+    }
+}
